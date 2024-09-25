fd5fd8eef035c46d55ba2cfdfdc6e1fe4ebb86cf
ahmadyani055 <147641402+ahmadyani055@users.noreply.github.com>
2024-09-25 19:28:42 +0700

    Add files via upload

---
+++ Posstest_PEMWEB_3/script.js
@@ -0,0 +1,8 @@
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+}
+
+function toggleMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
